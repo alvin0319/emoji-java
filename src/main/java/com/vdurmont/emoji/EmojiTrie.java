a79759818a144c7886f0d443dc92a97ec83e2fe9
Florian Spieß <business@minnced.club>
2021-12-19 20:05:03 +0100

    Update docs for EmojiTrie

--- src/main/java/com/vdurmont/emoji/EmojiTrie.java
+++ src/main/java/com/vdurmont/emoji/EmojiTrie.java
@@ -1,6 +1,7 @@
 package com.vdurmont.emoji;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.HashMap;
@@ -31,18 +32,15 @@ public class EmojiTrie {
     /**
      * Checks if sequence of chars contain an emoji.
      *
-     * @param sequence Sequence of char that may contain emoji in full or
-     *                 partially.
-     * @return &lt;li&gt;
-     * Matches.EXACTLY if char sequence in its entirety is an emoji
-     * &lt;/li&gt;
-     * &lt;li&gt;
-     * Matches.POSSIBLY if char sequence matches prefix of an emoji
-     * &lt;/li&gt;
-     * &lt;li&gt;
-     * Matches.IMPOSSIBLE if char sequence matches no emoji or prefix of an
-     * emoji
-     * &lt;/li&gt;
+     * @param sequence
+     *        Sequence of char that may contain emoji in full or partially.
+     *
+     * @return
+     * <ul>
+     *  <li>{@link Matches#EXACTLY} if char sequence in its entirety is an emoji</li>
+     *  <li>{@link Matches#POSSIBLY} if char sequence matches prefix of an emoji</li>
+     *  <li>{@link Matches#IMPOSSIBLE} if char sequence matches no emoji or prefix of an emoji</li>
+     * </ul>
      */
     @NotNull
     public Matches isEmoji(char[] sequence) {
@@ -51,6 +49,23 @@ public class EmojiTrie {
 
     /**
      * Checks if the sequence of chars within the given bound indices contain an emoji.
+     *
+     * @param sequence
+     *        Sequence of char that may contain emoji in full or partially.
+     * @param start
+     *        The starting index
+     * @param end
+     *        The end index (exclusive)
+     *
+     * @throws ArrayIndexOutOfBoundsException
+     *         If the provided range is invalid
+     *
+     * @return
+     * <ul>
+     *  <li>{@link Matches#EXACTLY} if char sequence in its entirety is an emoji</li>
+     *  <li>{@link Matches#POSSIBLY} if char sequence matches prefix of an emoji</li>
+     *  <li>{@link Matches#IMPOSSIBLE} if char sequence matches no emoji or prefix of an emoji</li>
+     * </ul>
      *
      * @see #isEmoji(char[])
      */
@@ -76,18 +91,21 @@ public class EmojiTrie {
         return tree.isEndOfEmoji() ? Matches.EXACTLY : Matches.POSSIBLY;
     }
 
-
     /**
      * Finds Emoji instance from emoji unicode
      *
-     * @param unicode unicode of emoji to get
+     * @param  unicode
+     *         unicode of emoji to get
+     *
      * @return Emoji instance if unicode matches and emoji, null otherwise.
      */
-    public Emoji getEmoji(String unicode) {
+    @Nullable
+    public Emoji getEmoji(@NotNull String unicode) {
         return getEmoji(unicode.toCharArray(), 0, unicode.length());
     }
 
-    Emoji getEmoji(char[] sequence, int start, int end) {
+    @NotNull
+    protected Emoji getEmoji(@NotNull char[] sequence, int start, int end) {
         if (start < 0 || start > end || end > sequence.length) {
             throw new ArrayIndexOutOfBoundsException(
                     "start " + start + ", end " + end + ", length " + sequence.length);
